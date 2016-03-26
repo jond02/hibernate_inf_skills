16721f3cf18fe4898f2dfb6eb825858ed8230ad6
Jonathan Dann <nwjon122@gmail.coml>
2016-03-25 21:52:54 -0700

    initial commit

---
+++ src/main/java/com/infskills/App.java
@@ -0,0 +1,13 @@
+package com.infskills;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/infskills/data/entities/User.java
@@ -0,0 +1,102 @@
+package com.infskills.data.entities;
+
+import java.util.Date;
+
+/**
+ * Created by jondann on 3/25/16.
+ */
+public class User {
+
+    private Long userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date birthDate;
+
+    private String emailAddress;
+
+    private Date lastUpdateDate;
+
+    private String lastUpdateBy;
+
+    private Date CreatedDate;
+
+    private String createdBy;
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy;
+    }
+
+    public Date getCreatedDate() {
+        return CreatedDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        CreatedDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+
+}
